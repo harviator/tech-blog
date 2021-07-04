bb1cf90fcff3cb61a5ba0cef69d0a4e9293439f0
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-04 17:37:53 -0400

    Added Post routes for update and delete

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Put route to update post
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+      const postData = await Post.update({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!postData) {
+        res.status(404).json({ message: 'No post found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
+
+// Delete route to delete post
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const postData = await Post.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!postData) {
+        res.status(404).json({ message: 'No post found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
