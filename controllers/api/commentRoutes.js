a882c4b8a621baf25f5396f4fc1d134340f51804
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-08 19:53:10 -0400

    Cleaned up server.js file

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
-// const withAuth = require('../../utils/auth');
 
-// Creat New Post Route
+// Create New Comment Route
 router.post('/', async (req, res) => {
     try {
       const newComment = await Comment.create({
@@ -14,52 +13,6 @@ router.post('/', async (req, res) => {
       res.status(200).json(newComment);
     } catch (err) {
       res.status(400).json(err);
-    }
-  });
-
-// Put route to update post
-router.put('/:id', async (req, res) => {
-    try {
-      const postData = await Post.update(req.body, {
-        where: {
-          id: req.params.id,
-          user_id: req.session.user_id,
-        },
-      });
-  
-      console.log(postData);
-
-      if (!postData) {
-        res.status(404).json({ message: 'No post found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(postData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  
-  module.exports = router;
-
-// Delete route to delete post
-router.delete('/:id', async (req, res) => {
-    try {
-      const postData = await Post.destroy({
-        where: {
-          id: req.params.id,
-          user_id: req.session.user_id,
-        },
-      });
-  
-      if (!postData) {
-        res.status(404).json({ message: 'No post found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(postData);
-    } catch (err) {
-      res.status(500).json(err);
     }
   });
   
