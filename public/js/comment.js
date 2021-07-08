21954af127188bbe67d864bcbe5e4c1fa66976c8
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-07 21:29:17 -0400

    Fixed some issues, but still have other problems

---
+++ public/js/comment.js
@@ -0,0 +1,27 @@
+console.log('Hello World');
+
+const newCommentFormHandler = async (event) => {
+    event.preventDefault();
+
+    const content = document.querySelector('#comment-content').value.trim();
+    const id = document.querySelector('#post-id').value;
+
+    if (content) {
+
+        const response = await fetch('/api/comments/', {
+            method: 'POST',
+            body: JSON.stringify({ content, id }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document
+    .querySelector('.form-new-comment')
+    .addEventListener('submit', newCommentFormHandler);
