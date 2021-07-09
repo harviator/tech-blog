7f00b5e6413a12e3928bb816ca2e8c0e5fbf048d
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-09 06:43:37 -0400

    Cleaned up and added comments to postRoutes.js file

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -4,63 +4,63 @@ const { Post } = require('../../models');
 
 // Creat New Post Route
 router.post('/', async (req, res) => {
-    try {
-      const newPost = await Post.create({
-        title: req.body.title,
-        content: req.body.content,
-        user_id: req.session.user_id,
-      });
-  
-      res.status(200).json(newPost);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+  try {
+    const newPost = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.user_id,
+    });
 
-// Put route to update post
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Update Post Route
 router.put('/:id', async (req, res) => {
-    try {
-      const postData = await Post.update(req.body, {
-        where: {
-          id: req.params.id,
-          user_id: req.session.user_id,
-        },
-      });
-  
-      console.log(postData);
+  try {
+    const postData = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
 
-      if (!postData) {
-        res.status(404).json({ message: 'No post found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(postData);
-    } catch (err) {
-      res.status(500).json(err);
+    console.log(postData);
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
     }
-  });
-  
-  module.exports = router;
 
-// Delete route to delete post
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
+
+// Delete Post Route
 router.delete('/:id', async (req, res) => {
-    try {
-      const postData = await Post.destroy({
-        where: {
-          id: req.params.id,
-          user_id: req.session.user_id,
-        },
-      });
-  
-      if (!postData) {
-        res.status(404).json({ message: 'No post found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(postData);
-    } catch (err) {
-      res.status(500).json(err);
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
     }
-  });
-  
-  module.exports = router;
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
