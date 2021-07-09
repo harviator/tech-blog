56aad5ee88729390dde39927fa18740112144c0c
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-09 09:54:54 -0400

    Made some changes and updates

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 
-require('dotenv').config();
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create();
+const hbs = exphbs.create({});
 
 const sess = {
   secret: process.env.SECRET,
