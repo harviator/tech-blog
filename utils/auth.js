e85e73a2224fceea626e0dbfcc97f8d5d278dc03
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-08 20:04:40 -0400

    Cleaned up auth.js file

--- utils/auth.js
+++ utils/auth.js
@@ -1,10 +1,10 @@
 const withAuth = (req, res, next) => {
-    if (!req.session.logged_in) {
-      res.redirect('/login');
-    } else {
-      console.log('passed')
-      next();
-    }
-  };
-  
-  module.exports = withAuth;
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    console.log('passed')
+    next();
+  }
+};
+
+module.exports = withAuth;
