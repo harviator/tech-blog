41c52b156333693b902a9f3d8b8f82e2c7445b17
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-08 20:05:53 -0400

    Cleaned up signup.js file

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,26 +1,26 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
     }
+  }
 
-    console.log('This is the signup');
-  };
+  console.log('This is the signup');
+};
 
-  document
+document
   .querySelector('.form-signup')
   .addEventListener('submit', signupFormHandler);
