c66101e1045e968b5bf35f1426349bfca445a1be
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-09 06:44:47 -0400

    Cleaned up index.js file is controllers > api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,6 @@ const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
 
-//localhost3001/api/post etc...
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
