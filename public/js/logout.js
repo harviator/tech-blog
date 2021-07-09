3da87f876cf9130c32a585a83d1e20d5b67fe3c2
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-08 20:06:36 -0400

    Cleaned up logout.js file

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,16 +1,16 @@
 const logout = async () => {
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
 
-    console.log('This is the logout');
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+
+  console.log('This is the logout');
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
