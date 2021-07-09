987b831afb97bb9bab30395c2ff6cb2e8ef3d80d
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-09 06:46:19 -0400

    Cleaned up commentRoutes.js file

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,17 +3,17 @@ const { Comment } = require('../../models');
 
 // Create New Comment Route
 router.post('/', async (req, res) => {
-    try {
-      const newComment = await Comment.create({
-        content: req.body.content,
-        user_id: req.session.user_id,
-        post_id: req.body.id
-      });
-  
-      res.status(200).json(newComment);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
-  
-  module.exports = router;
+  try {
+    const newComment = await Comment.create({
+      content: req.body.content,
+      user_id: req.session.user_id,
+      post_id: req.body.id
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
